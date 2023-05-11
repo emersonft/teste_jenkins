7627173b85992d702dcf5b89d8049e0002a01d44
Emersonft <emersonft@hotmail.com>
2023-05-11 11:21:33 -0300

    Teste 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Teste
+          Learn React Teste 2
         </a>
       </header>
     </div>
